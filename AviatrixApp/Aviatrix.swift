2fc7f93f11bd380e3af2e7ca59d43f5f5e3ab6ec
Apple <apple@Apples-MacBook-Pro-6.local>
2019-07-10 11:51:56 -0700

    Complete Iteration 0

--- AviatrixApp/Aviatrix.swift
+++ AviatrixApp/Aviatrix.swift
@@ -10,6 +10,13 @@ import Foundation
 
 class Aviatrix {
     
+    //who created this plane and airline
+    var author = ""
+    init(myAuthor: String) {
+        author = myAuthor
+    }
+    var running = false
+    // bool stands for boolean which means true or false
     func start() -> Bool {
         return true
     }
